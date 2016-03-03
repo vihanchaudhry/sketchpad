2a9278d47eb515eb20ba423f4ddaf378159e5312
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-03-02 22:34:35 -0800

    Sketching done, resizing done (button needed)

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-var rows = 16;
-var cols = 16;
+var width = 960;
+var rows = 14;
+var cols = 10;
+var height = Math.floor(width * cols / rows);
+var blockWidth = width / rows;
+var blockHeight = height / cols;
+
 var $row = $('<div/>', {
 	class: 'row'
 });
+$row.height(blockHeight);
+
 var $block = $('<div/>', {
 	class: 'block'
 });
+$block.width(blockWidth);
+$block.height(blockHeight);
 
 $(document).ready(function() {
 	for (var i = 0; i < rows; i++) {
@@ -15,4 +24,8 @@ $(document).ready(function() {
 	for (var j = 0; j < cols; j++) {
 		$('body').append($row.clone());
 	}
+
+	$('.block').mouseenter(function() {
+		$(this).css('background-color', '#c6c6c6');
+	});
 });
