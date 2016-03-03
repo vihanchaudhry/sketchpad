496d92d87f5d543b826c279387489ab256106f5e
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-03-03 06:54:03 -0800

    Finished header and reset

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-var width = 960;
-var rows = 14;
-var cols = 10;
-var height = Math.floor(width * cols / rows);
-var blockWidth = width / rows;
-var blockHeight = height / cols;
 
-var $row = $('<div/>', {
-	class: 'row'
-});
-$row.height(blockHeight);
 
-var $block = $('<div/>', {
-	class: 'block'
-});
-$block.width(blockWidth);
-$block.height(blockHeight);
+var reset = function() {
+	var width = 960;
+	var rows = prompt("How many rows?");
+	var cols = prompt("How many columns?");
+	var height = Math.floor(width * cols / rows);
+	var blockWidth = width / rows;
+	var blockHeight = height / cols;
 
-$(document).ready(function() {
+	var $row = $('<div/>', {
+		class: 'row'
+	});
+	$row.height(blockHeight);
+
+	var $block = $('<div/>', {
+		class: 'block'
+	});
+	$block.width(blockWidth);
+	$block.height(blockHeight);
+
 	for (var i = 0; i < rows; i++) {
 		$row.append($block.clone());
 	}
@@ -28,4 +30,11 @@ $(document).ready(function() {
 	$('.block').mouseenter(function() {
 		$(this).css('background-color', '#c6c6c6');
 	});
+};
+
+$(document).ready(function() {
+	reset();
+
+	$(document).getElementById('reset').onclick = reset();
 });
+
